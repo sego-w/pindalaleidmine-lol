67ce602bba6a55744b57536e8780cc5037a3c84c
Karl-Martin <karlmartin.lokk@gmail.com>
2023-05-05 09:24:28 +0300

    chomp

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.urls import include
+
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+]
