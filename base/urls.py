b9bfa26df350e944360844ffbd50da55d28732c3
Karl-Martin <karlmartin.lokk@gmail.com>
2023-06-11 23:28:38 +0300

    MIDAGI ACTUALLY TÖÖTAB HOLY FLIP
    chatgpt so real for this one :100:

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, include
-from . import views
-from django.conf import settings
-from django.urls import include
-
+from django.urls import path
+from .views import ShapeDrawerView
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('canvas/', views.canvas_view, name='canvas'),
-    path('description', views.description, name = 'mushamusha'),
+    path('', ShapeDrawerView.as_view(), name='shape_drawer'),
+    path('', ShapeDrawerView.as_view(), name='calculate_area'),
 ]
